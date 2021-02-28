ced321a1bdeadd247adc06b808e694642ac4ecd2
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-02-28 16:45:13 +0000

    removes ordering from queries

--- api_v1/api_handler/constants.py
+++ api_v1/api_handler/constants.py
@@ -217,7 +217,6 @@ WHERE
       metric = ANY($$1::VARCHAR[])
   AND rr.released IS TRUE
   $filters
-ORDER BY date, area_code
 LIMIT $limit OFFSET $offset
 """)
 
@@ -253,8 +252,7 @@ WHERE
         AND metric = '$latest_by'
         AND (payload ->> 'value') NOTNULL
         $filters
-  )
-ORDER BY date, area_code""")
+  )""")
 
     # noinspection SqlResolve,SqlNoDataSourceInspection
     exists = Template("""\
