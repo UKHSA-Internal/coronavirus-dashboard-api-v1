b9113bb82f52830f39e9f57b994814175ed8da46
Jaroslaw Michalski <jaroslaw.m.michalski@phe.gov.uk>
2023-03-14 09:53:29 +0000

    add new metrics to DATA_TYPES

--- api_v1/api_handler/constants.py
+++ api_v1/api_handler/constants.py
@@ -421,6 +421,11 @@ DATA_TYPES: Dict[str, Callable[[str], Any]] = {
     "uniqueCasePositivityBySpecimenDateRollingSum": float,
     "uniquePeopleTestedBySpecimenDateRollingSum": int,
 
+    "newDailyNsoDeathsByDeathDateChange": int,
+    "newDailyNsoDeathsByDeathDateChangePercentage": float,
+    "newDailyNsoDeathsByDeathDateDirection": str,
+    "newDailyNsoDeathsByDeathDateRollingSum": int,
+
     "newDailyNsoDeathsByDeathDate": int,
     "cumDailyNsoDeathsByDeathDate": int,
 
@@ -776,6 +781,11 @@ if ENVIRONMENT == "DEVELOPMENT":
         "cumPeopleReceivingFirstDose": int,
         "newPeopleReceivingSecondDose": int,
         "cumPeopleReceivingSecondDose": int,
+
+        "newDailyNsoDeathsByDeathDateChange": int,
+        "newDailyNsoDeathsByDeathDateChangePercentage": float,
+        "newDailyNsoDeathsByDeathDateDirection": str,
+        "newDailyNsoDeathsByDeathDateRollingSum": int,
 
         "cumWeeklyNsoDeathsByRegDate": int,
         "cumWeeklyNsoDeathsByRegDateRate": float,
