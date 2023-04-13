15d76a6f83640d603ee713218f287d57e9baf0d2
olaadejnr <olaadejnr@yahoo.com>
2023-04-13 18:13:08 +0100

    UPDATE: removed sandbox env

--- processor_settings/settings.py
+++ processor_settings/settings.py
@@ -16,7 +16,6 @@ __all__ = [
     "ENV_KEY",
     "DEV_ENV",
     "PROD_ENV",
-    "SANDBOX_ENV"
     "ENVIRONMENT",
     "URL_SEPARATOR",
     "JSON_SEPARATORS",
@@ -27,14 +26,12 @@ __all__ = [
 ENV_KEY = "API_ENV"
 DEV_ENV = "DEVELOPMENT"
 STAGING_ENV = "STAGING"
-SANDBOX_ENV = "SANDBOX"
 PROD_ENV = "PRODUCTION"
 
 
 ENV_GITHUB_BRANCH_MAP = {
     DEV_ENV: "development",
     STAGING_ENV: "staging",
-    SANDBOX_ENV: "sandbox",
     PROD_ENV: "master"
 }
 
